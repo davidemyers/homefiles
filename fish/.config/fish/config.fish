f8850bd6368505c97fc5547d3c88e2fc8397fb7c
David Myers <dem@myersnet.net>
2025-05-14 15:16:01 -0400

    fish: Add 'functions' directory to 'check' function.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -205,7 +205,7 @@ if status is-interactive
     # For this to work you need keep this file formatted the way fish_indent
     # tells you to.
     function check --description 'Check the fish config file(s)'
-        for config in $__fish_config_dir/config.fish $__fish_config_dir/conf.d/*.fish
+        for config in $__fish_config_dir/config.fish $__fish_config_dir/conf.d/*.fish $__fish_config_dir/functions/*.fish
             if not fish_indent --check $config
                 diff $config (fish_indent $config | psub)
                 break
