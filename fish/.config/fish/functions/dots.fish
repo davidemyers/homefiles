2b7247dd94f2da431dbc3c24576d24493d81fd95
David Myers <dem@myersnet.net>
2025-05-15 11:25:44 -0400

    fish: Make 'dots' function smarter.

--- fish/.config/fish/functions/dots.fish
+++ fish/.config/fish/functions/dots.fish
@@ -1,22 +1,27 @@
 # Manage my common home directory files, which I store on GitHub for easy
 # external access.
 function dots --description 'Make sure .homefiles are current'
-    if path is -d ~/.homefiles; and command -q stow
-        pushd ~/.homefiles
-        if path is .gitignore
-            # This is the master copy, just print status.
-            git status
-        else
-            git pull
-            switch (uname)
-                case Linux
-                    stow --no-folding bash fish nano sup tmux
-                case Darwin
-                    stow --no-folding fish
-            end
-        end
-        popd
-    else
-        echo This function is not available on this system.
+    set repo ~/.homefiles
+    if not path is -d $repo
+        echo Directory $repo is missing.
+        return 1
     end
+    if not command -q stow
+        echo Command \'stow\' is not installed.
+        return 1
+    end
+    pushd $repo
+    if path is .gitignore
+        # This is the master copy, just print status.
+        git status
+    else
+        git pull
+        switch (uname)
+            case Linux
+                stow --no-folding bash fish nano sup tmux
+            case Darwin
+                stow --no-folding fish
+        end
+    end
+    popd
 end
