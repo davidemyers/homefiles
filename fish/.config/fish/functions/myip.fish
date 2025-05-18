85f19cb54eb74dc02a3ed31823e0222193487034
David Myers <dem@myersnet.net>
2025-05-18 16:44:27 -0400

    fish: Make 'myip' function more robust.

--- fish/.config/fish/functions/myip.fish
+++ fish/.config/fish/functions/myip.fish
@@ -1,4 +1,19 @@
+# Find this system's public IP addresses.
 function myip --description 'Print public IP addresses'
-    curl -4 icanhazip.com
-    curl -6 icanhazip.com
+    set target icanhazip.com
+    if not command -q curl
+        echo Command \'curl\' is not installed.
+        return 1
+    end
+    curl -4 $target
+    switch (uname)
+        case Linux
+            if ip -6 route get ::/128 &>/dev/null
+                curl -6 $target
+            end
+        case Darwin
+            if route get -inet6 ::/128 &>/dev/null
+                curl -6 $target
+            end
+    end
 end
