ededa7eb794ce275a5eb104ee99415fdc155015a
David Myers <dem@myersnet.net>
2025-05-26 15:57:50 -0400

    fish: Rename 'check' to 'check-fish'.

--- fish/.config/fish/functions/check-fish.fish
+++ fish/.config/fish/functions/check-fish.fish
@@ -1,7 +1,7 @@
 # Check all fish configuration files for syntax and formatting errors. For
 # this to work you need keep the files formatted the way fish_indent tells you
 # to.
-function check --description 'Check the fish config file(s)'
+function check-fish --description 'Check the fish config file(s)'
     for config in $__fish_config_dir/config.fish $__fish_config_dir/conf.d/*.fish $__fish_config_dir/functions/*.fish
         if not fish_indent --check $config
             diff $config (fish_indent $config | psub)
